867771a44c6041ce2ce35de2b8fbd892ade1a3a6
Byron Sebastian Rosero Diaz <sebasdiaz67@gmail.com>
2020-06-26 10:59:41 -0500

    * Firts commit project employee votes.

---
+++ avalith-employee-votes/src/main/java/com/avalith/votes/AvalithEmployeeVotesApplication.java
@@ -0,0 +1,13 @@
+package com.avalith.votes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvalithEmployeeVotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvalithEmployeeVotesApplication.class, args);
+	}
+
+}
---
+++ avalith-employee-votes/src/test/java/com/avalith/votes/AvalithEmployeeVotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avalith.votes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvalithEmployeeVotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
